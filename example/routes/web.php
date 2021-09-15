4a5684ad2845fe7b42451e4bbdadf743b4bae16e
Abhishekjecrc <73646495+Abhishekjecrc@users.noreply.github.com>
2021-09-15 23:34:50 +0530

    Update web.php

--- example/routes/web.php
+++ example/routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\productdetails;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|ffff
 */
 
 Route::get('/', function () {
